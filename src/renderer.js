5d87fc5796f7fb08240157441518d6877ab23da0
Óscar Vega <s16120245@alumnos.itsur.edu.mx>
2019-05-22 22:17:10 -0500

    Más cambios

--- src/renderer.js
+++ src/renderer.js
@@ -41,14 +41,21 @@ ipcRenderer.on('cerial', (event, datos) => {
           break;
           case "LDR":
             ldr = document.querySelector(".dot");
-          if(aro[1] <= 25){
+            if(aro[1] <= 5){
+              ldr.style.boxShadow = `0 0 10px #000, 0 0 20px #000, 0 0 30px #6A6A6A, 0 0 20px #6A6A6A, 0 0 50px #6A6A6A, 0 0 60px #6A6A6A, 0 0 70px #6A6A6A`;
+              ldr.style.backgroundColor = `#6A6A6A`;
+          }else if(aro[1] > 5 && aro[1] <= 25){
             ldr.style.boxShadow = `0 0 10px #fff, 0 0 20px #fff, 0 0 30px #FFED00, 0 0 20px #FFED00, 0 0 50px #FFED00, 0 0 60px #FFED00, 0 0 70px #FFED00`;
+            ldr.style.backgroundColor = `#FFF9B2`;
           }else if(aro[1] > 25 && aro[1] <= 50){
             ldr.style.boxShadow = `0 0 20px #fff, 0 0 30px #fff, 0 0 40px #FFF142, 0 0 30px #FFF142, 0 0 60px #FFF142, 0 0 70px #FFF142, 0 0 80px #FFF142`;
+            ldr.style.backgroundColor = `#FFF9B2`;
           }else if(aro[1] > 50 && aro[1] <= 75){
             ldr.style.boxShadow = `0 0 30px #fff, 0 0 40px #fff, 0 0 50px #FFF57F, 0 0 40px #FFF57F, 0 0 70px #FFF57F, 0 0 80px #FFF57F, 0 0 90px #FFF57F`;
+            ldr.style.backgroundColor = `#FFF9B2`;
           }else{
             ldr.style.boxShadow = `0 0 40px #fff, 0 0 50px #fff, 0 0 60px #FFF9B2, 0 0 50px #FFF9B2, 0 0 80px #FFF9B2, 0 0 90px #FFF9B2, 0 0 100px #FFF9B2`;
+            ldr.style.backgroundColor = `#FFF9B2`;
           }
           break;
         }
